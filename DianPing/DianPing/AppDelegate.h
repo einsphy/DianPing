9026a0d700c6920da5b4de2107abde20fdf9c866
einsphy <einsphy@163.com>
2016-03-29 18:19:50 +0800

    增加cocoapods

---
+++ DianPing/DianPing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DianPing
+//
+//  Created by einsphy on 16/3/29.
+//  Copyright © 2016年 einsphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
